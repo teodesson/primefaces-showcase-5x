e6a898bc265e85780c37e9f92545a39978ed1d02
teodesson <teodesson@gmail.com>
2014-06-05 16:49:26 +0800

    Fix Menu

--- src/main/java/org/primefaces/examples/view/MenuBean.java
+++ src/main/java/org/primefaces/examples/view/MenuBean.java
@@ -15,66 +15,73 @@
  */
 package org.primefaces.examples.view;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import org.primefaces.component.menuitem.UIMenuItem;
-import org.primefaces.component.submenu.UISubmenu;
+import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
 import org.primefaces.model.menu.MenuModel;
 
 public class MenuBean {
 
-	private MenuModel model;
-
-	public MenuBean() {
-		model = new DefaultMenuModel();
-		
-		//First submenu
-		UISubmenu submenu = new UISubmenu();
-		submenu.setLabel("Dynamic Submenu 1");
-		
-		UIMenuItem item = new UIMenuItem();
-		item.setValue("Dynamic Menuitem 1.1");
-		item.setUrl("#");
-		submenu.getChildren().add(item);
-		
-		model.addElement(submenu);
-		
-		//Second submenu
-		submenu = new UISubmenu();
-		submenu.setLabel("Dynamic Submenu 2");
-		
-		item = new UIMenuItem();
-		item.setValue("Dynamic Menuitem 2.1");
-		item.setUrl("#");
-		submenu.getChildren().add(item);
-		
-		item = new UIMenuItem();
-		item.setValue("Dynamic Menuitem 2.2");
-		item.setUrl("#");
-		submenu.getChildren().add(item);
-		
-		model.addElement(submenu);
-	}
-
-	public MenuModel getModel() {
-		return model;
-	}	
-    
+    private MenuModel model;
+ 
+    @PostConstruct
+    public void init() {
+        model = new DefaultMenuModel();
+         
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+         
+        DefaultMenuItem item = new DefaultMenuItem("External");
+        item.setUrl("http://www.primefaces.org");
+        item.setIcon("ui-icon-home");
+        firstSubmenu.addElement(item);
+         
+        model.addElement(firstSubmenu);
+         
+        //Second submenu
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+ 
+        item = new DefaultMenuItem("Save");
+        item.setIcon("ui-icon-disk");
+        item.setCommand("#{menuView.save}");
+        item.setUpdate("messages");
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("ui-icon-close");
+        item.setCommand("#{menuView.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+         
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("ui-icon-search");
+        item.setCommand("#{menuView.redirect}");
+        secondSubmenu.addElement(item);
+ 
+        model.addElement(secondSubmenu);
+    }
+ 
+    public MenuModel getModel() {
+        return model;
+    }   
+     
     public void save() {
-		addMessage("Data saved");
-	}
-	
-	public void update() {
-		addMessage("Data updated");
-	}
-	
-	public void delete() {
-		addMessage("Data deleted");
-	}
-	
-	public void addMessage(String summary) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	}
+        addMessage("Success", "Data saved");
+    }
+     
+    public void update() {
+        addMessage("Success", "Data updated");
+    }
+     
+    public void delete() {
+        addMessage("Success", "Data deleted");
+    }
+     
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 }
